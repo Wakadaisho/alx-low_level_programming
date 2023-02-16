8eff89fb7d3608047085ef5a22c6a53ddee80d5d
Wakadaisho <paulwahome9@gmail.com>
2023-02-16 15:30:05 +0300

    Correct length requirement for task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,9 +11,9 @@
  */
 int main(void)
 {
-	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(STDERR_FILENO, text, sizeof(text) / sizeof(char));
+	write(STDERR_FILENO,  
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+		59);
 
 	return (1);
 }
