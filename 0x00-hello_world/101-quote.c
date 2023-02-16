a40797a1fadc70720c582c9aeb8b76702d8a3788
Wakadaisho <paulwahome9@gmail.com>
2023-02-16 15:31:43 +0300

    Remove whitespace to fit betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,8 +11,8 @@
  */
 int main(void)
 {
-	write(STDERR_FILENO,  
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+	write(STDERR_FILENO,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 		59);
 
 	return (1);
