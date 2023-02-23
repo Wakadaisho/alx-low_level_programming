12f1c07662b10a3c6aef2831bf0e7f78915c4963
Wakadaisho <paulwahome9@gmail.com>
2023-02-23 19:25:45 +0300

    Add main.h to .gitignore

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,27 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+int _isupper(int);
+
+int _isdigit(int);
+
+int mul(int, int);
+
+int _putchar(char);
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int);
+
+void print_diagonal(int);
+
+void print_square(int);
+
+void print_triangle(int);
+
+void print_number(int);
+#endif
