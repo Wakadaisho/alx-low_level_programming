9e64984a89512fb1026bb8bb28492c7cca896af5
Wakadaisho <paulwahome9@gmail.com>
2023-03-22 15:04:03 +0300

    Add docs to header files

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,7 @@
 #ifndef _CALC_H_
 #define _CALC_H_
 #include <stddef.h>
+
 /**
  * struct op - Struct op
  *
@@ -13,15 +14,63 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
+/**
+ * op_add - return the sum of two integers
+ *
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: integer
+ */
 int op_add(int a, int b);
 
+/**
+ * op_sub - return the difference between two integers
+ *
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: integer
+ */
 int op_sub(int a, int b);
 
+/**
+ * op_mul - return the multiple of two integers
+ *
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: integer
+ */
 int op_mul(int a, int b);
 
+/**
+ * op_div - return the integer ratio of two integers
+ *
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: integer
+ */
 int op_div(int a, int b);
 
+/**
+ * op_mod - return the mod value betwen two integers
+ *
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: integer
+ */
 int op_mod(int a, int b);
 
+/**
+ * get_op_func - return a function corresponding to an operator
+ *
+ * @s: operator to match a function to
+ *
+ * Return: int (*)(int, int) - function that was as a binary operator
+ */
 int (*get_op_func(char *s))(int, int);
+
 #endif /* _CALC_H_ */
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,37 @@
-#ifndef __FUNCTION_POINTERS_H_
+#ifndef __FUNCTION_POINTERS_H__
 #define __FUNCTION_POINTERS_H__
 #include <stddef.h>
 
+/**
+ * print_name - prints out a name, using a function pointer
+ *
+ * @name: string to print
+ * @f: function pointer that specifies how the printing is done
+ *
+ * Return: void
+ */
 void print_name(char *name, void (*f)(char *));
 
+/**
+ * array_iterator - execute a function on each element in an array
+ *
+ * @array: the array to process
+ * @size: size of the array
+ * @action: function to execute
+ *
+ * Return: void
+ */
 void array_iterator(int *array, size_t size, void (*action)(int));
 
+/**
+ * int_index - find a number in an array using a comparison function pointer
+ *
+ * @array: the array to process
+ * @size: size of the array
+ * @cmp: function used to compare values
+ *
+ * Return: int, index of found element
+ *		-1 if not found
+ */
 int int_index(int *array, int size, int (*cmp)(int));
-#endif /* __FUNCTION_POINTERS_H_ */
+#endif /* __FUNCTION_POINTERS_H__ */
