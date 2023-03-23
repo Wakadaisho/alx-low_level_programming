7ad3aaeaeb96cf6ca6deb3ec70936585e6bf11ad
Wakadaisho <paulwahome9@gmail.com>
2023-03-23 20:50:23 +0300

    Fix printf parameters

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdio.h>
 
 /**
  * print_numbers - print out the numbers passed
@@ -12,7 +13,7 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
-	unsigned int i;
+	unsigned int i = 0;
 	const char *s = separator == NULL ? "" : separator;
 
 	if (n == 0)
@@ -24,7 +25,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	while (i < n - 1)
 	{
-		printf("%i%s", va_arg(args, int);
+		printf("%i%s", va_arg(args, int), s);
 		i++;
 	}
 
