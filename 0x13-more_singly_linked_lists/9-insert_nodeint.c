a65f894a04ea9703069889a9dbff22390286419d
Wakadaisho <paulwahome9@gmail.com>
2023-04-04 19:26:43 +0300

    Different refactoring

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * insert_nodeint_at_index - inserts a node with data at a certain index
@@ -13,39 +14,36 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	listint_t *p, *new, *prev = NULL;
+	listint_t *p, *new, *prev;
 
 	if (head == NULL)
 		return (NULL);
 
-	p = *head;
-	prev = *head;
-
-	while (i++ < idx)
-	{
-		prev = p;
-		p = p->next;
-		if (!p)
-			return (NULL);
-	}
-
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	new->next = p;
 	new->n = n;
-	if (p == NULL && idx == 0)
+	new->next = NULL;
+
+	if (idx == 0)
 	{
+		if (*head)
+			new->next = *head;
 		*head = new;
+		return (new);
 	}
-	else if (p == NULL && idx != 0)
+
+	for (p = *head; i < idx && p; i++, p = p->next)
+		prev = p;
+
+	if (!p && i != idx)
 	{
 		free(new);
 		return (NULL);
 	}
-	else
-	{
-		prev->next = new;
-	}
+
+	new->next = p;
+	prev->next = new;
+
 	return (new);
 }
