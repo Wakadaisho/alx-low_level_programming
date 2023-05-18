bc8bd38458776938b0a12b2dbd55ba9b68c4a4c1
Wakadaisho <paulwahome9@gmail.com>
2023-05-18 18:20:48 +0300

    Fix max value checking logic

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -5,9 +5,10 @@ def isPalindrome(n):
     return string == string[::-1]
 
 
-max = (0, 0)
-for i in range(100, 1000):
-    for j in range(i, 1000):
-        if (isPalindrome(i*j)):
-            max = (i, j)
-print(f"{max[0] * max[1]} = {max[0]} x {max[1]}")
+if __name__ == "__main__":
+    max = (0, 0)
+    for i in range(100, 1000):
+        for j in range(i, 1000):
+            if (isPalindrome(i*j) and i * j > max[0] * max[1]):
+                max = (i, j)
+    print(f"{max[0] * max[1]} = {max[0]} x {max[1]}")
