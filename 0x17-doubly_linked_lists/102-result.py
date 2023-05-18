17a24bd79dab62cce5dc16e54b3c103595086611
Wakadaisho <paulwahome9@gmail.com>
2023-05-18 18:10:31 +0300

    Make palindrome calculation more efficient

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 def isPalindrome(n):
-    string = str(n);
+    string = str(n)
     return string == string[::-1]
 
-max = (0,0)
-for i in range(100,1000):
-	for j in range(100,1000):
-		if (isPalindrome(i*j)):
-			max = (i, j)
 
-print(max[0]*max[1])
+max = (0, 0)
+for i in range(100, 1000):
+    for j in range(i, 1000):
+        if (isPalindrome(i*j)):
+            max = (i, j)
+print(f"{max[0] * max[1]} = {max[0]} x {max[1]}")
