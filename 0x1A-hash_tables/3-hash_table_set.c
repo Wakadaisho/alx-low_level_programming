2a870d93a4b1bd3f25551a8a65cde165cb32f596
Wakadaisho <paulwahome9@gmail.com>
2023-06-09 17:28:40 +0300

    Remove unused variable

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -53,7 +53,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
  */
 hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
 {
-	hash_node_t *new, *node;
+	hash_node_t *new;
 
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
